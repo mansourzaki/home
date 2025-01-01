9f4c9b34ff2373a10a534c081a0d99a6991a9396
mansourzaki <mansourzaki2000@gmail.com>
2025-01-01 18:53:55 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,7 +20,7 @@ const Footer = (props) => {
         using <i className="fab fa-react" /> */}
         <p>
           <small className="text-muted">
-          Project forked from and edited from <a
+          Project forked and edited from <a
           rel="noopener"
           href="https://github.com/hashirshoaeb"
           aria-label="My GitHub"
